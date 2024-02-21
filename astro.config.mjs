7270119137e5c80762b76ac41befc75d7d10d7b3
Scott <2660603+ShoGinn@users.noreply.github.com>
2024-02-21 12:03:41 -0500

    build(deps): add netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import { defineConfig } from "astro/config";
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://astronetix.com",
   integrations: [tailwind(), icon()],
+  adapter: netlify(),
 });
