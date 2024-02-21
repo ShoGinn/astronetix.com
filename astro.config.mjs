48d8be8a47f43fc505610866aa497a9f0f5040ab
Scott <2660603+ShoGinn@users.noreply.github.com>
2024-02-21 12:17:53 -0500

    build(deps): update deps

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import { defineConfig } from "astro/config";
-import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://astronetix.com",
   integrations: [tailwind(), icon()],
-  adapter: netlify(),
 });
