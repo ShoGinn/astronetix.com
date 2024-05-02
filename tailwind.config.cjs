1ebf784748c1c331ac94df61691806f7142523c5
Scott <2660603+ShoGinn@users.noreply.github.com>
2024-05-02 11:11:07 -0400

    refactor: clean up code

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,29 +1,29 @@
 module.exports = {
-  content: ["./src/**/*.{astro,html,js,jsx,svelte,ts,tsx,vue}"],
+  content: ['./src/**/*.{astro,html,js,jsx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
       fontFamily: {
-        sans: ["var(--font-inter)"],
+        sans: ['var(--font-inter)'],
       },
       colors: {
-        primary: "var(--color-primary)",
-        secondary: "var(--color-secondary)",
+        primary: 'var(--color-primary)',
+        secondary: 'var(--color-secondary)',
       },
       textColor: {
-        default: "var(--color-text)",
-        offset: "var(--color-text-offset)",
+        default: 'var(--color-text)',
+        offset: 'var(--color-text-offset)',
       },
       backgroundColor: {
-        default: "var(--color-background)",
-        offset: "var(--color-background-offset)",
+        default: 'var(--color-background)',
+        offset: 'var(--color-background-offset)',
       },
       borderColor: {
-        default: "var(--color-border)",
+        default: 'var(--color-border)',
       },
     },
   },
   corePlugins: {
     fontSize: false,
   },
-  plugins: [require("tailwindcss-fluid-type")],
+  plugins: [require('tailwindcss-fluid-type')],
 };
